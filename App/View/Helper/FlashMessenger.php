4e97a05b410fd8b27023e106bf676831fad393d4
Dmitri Ischenko <ischenkodv@gmail.com>
2010-12-04 01:14:28 +0200

    Flash messenger added.

--- App/View/Helper/FlashMessenger.php
+++ App/View/Helper/FlashMessenger.php
@@ -4,25 +4,12 @@
  */
 class App_View_Helper_FlashMessenger extends Zend_View_Helper_Placeholder_Container_Standalone
 {
-
     /**
-     * @var string registry key
+     * View namespace.
+     * 
+     * @var type string
      */
-	protected $_regKey = 'App_View_Helpers_FlashMessenger';
-
-    /**
-     * List of messages
-     *
-     * @var array
-     */
-	protected $_messages = array();
-
-    protected $_namespaces = array(
-        'success',
-        'error',
-        'notice',
-        'warning'
-    );
+    protected $_namespace;
 
     /**
      * Prefix that is prepended before every class name of the message block
@@ -34,60 +21,96 @@ class App_View_Helper_FlashMessenger extends Zend_View_Helper_Placeholder_Contai
     /**
      * Instance of FlashMessenger
      *
-     * @var Zend_Controller_Action_Helper_FlashMessenger
+     * @var App_Controller_Action_Helper_FlashMessenger
      */
     protected $_flashMessenger;
 
     public function __construct()
     {
         parent::__construct();
-
-        // create instance of FlashMessenger
-        $this->_flashMessenger = new Zend_Controller_Action_Helper_FlashMessenger();
-
-        // loop through namespaces and collect messages
-        foreach ($this->_namespaces as $namespace) {
-            $this->_flashMessenger->setNamespace($namespace);
-            $this->_messages[$namespace] = $this->_flashMessenger->getMessages();
-        }
+        
+        $this->_flashMessenger = new App_Controller_Action_Helper_FlashMessenger();
     }
 
-    public function flashMessenger()
+    public function flashMessenger($namespace = null)
     {
+        $this->setNamespace($namespace !== null ? $namespace : 'default');
+        $this->_namespace = $namespace;
+        
     	return $this;
     }
+    
+    /**
+     * Set namespace for flash messenger.
+     * 
+     * @param string $namespace 
+     */
+    public function setNamespace($namespace)
+    {
+        $this->_flashMessenger->setNamespace($namespace);
+    }
+    
 
     /**
-     * Render messages
+     * Add message in flash messenger.
+     * 
+     * @param string $msg
+     */
+	public function addMessage($msg)
+	{
+        $this->_flashMessenger->addMessage($msg);
+	}
+
+    /**
+     * Render messages.
      *
      * @param string|int $indent
      * @return string
      */
     public function toString($indent = null)
     {
-        $indent = (null !== $indent)
-                ? $this->getWhitespace($indent)
-                : $this->getIndent();
-
-        $xhtml = $this->getPrefix();
-
-        if (count($this->_messages)) {
-            foreach ($this->_messages as $namespace=>$messages) {
-                if (count($messages)) {
-                    $xhtml .= '<ul class="'.$this->_classPrefix.ucfirst($namespace).'">';
-
-                    foreach ($messages as $m) {
-                        $xhtml .= '<li>'.$m.'</li>';
-                    }
-
-                    $xhtml .= '</ul>';
-                }
-            }
+        $str = $this->getPrefix();
+        
+        if ($this->_namespace) {
+            $namespaces = array($this->_namespace);
+        } else {
+            $namespaces = $this->_flashMessenger->getAvailableNamespaces();
         }
 
-        $xhtml .= $this->getPostfix();
+        foreach ($namespaces as $namespace) {
+            $str .= $this->render($namespace);
+        }
 
-        return $indent.$xhtml;
+        $str .= $this->getPostfix();
+
+        return $indent.$str;
+    }
+    
+    /**
+     * Render particular namespace.
+     * 
+     * @param type $namespace
+     * @return string 
+     */
+    public function render($namespace)
+    {
+        $str = '';
+        
+        $this->_flashMessenger->setNamespace($namespace);
+        $messages = $this->_flashMessenger->getMessages();
+
+        if (count($messages) > 0) {
+
+            $str .= '<ul class="'.$this->_classPrefix.ucfirst($namespace).'">';
+
+            foreach ($messages as $message) {
+                $str .= '<li>'.$message.'</li>';
+            }
+
+            $str .= '</ul>';
+        }
+        
+        return $str;
     }
 
 }
